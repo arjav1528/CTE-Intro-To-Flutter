36bb9131278cac8c3f36c4f96b8f028bbeab0da3
Arjav Patel <arjav1528@gmail.com>
2024-09-20 21:58:02 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: avoid_print
+
+import 'package:flutter/material.dart';
+void main(){
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text('data'),
+          centerTitle: true ,
+
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(30, 10, 30, 40),
+          child: Container(
+            height: 300,
+            width: 200,
+            decoration: const BoxDecoration(
+                color: Colors.blue
+            ),
+            child: TextButton(
+              onPressed: (){
+                print('Text Button Pressed');
+              },
+              child: const Text('CTE'),
+            )
+            ,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            print('Button Pressed');
+          },
+        ),
+
+      ),
+    );
+  }
+}
